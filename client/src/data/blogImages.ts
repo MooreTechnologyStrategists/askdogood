d05118dde8d39a147b371f5049458699fd5bf10a
RoSeé Murphy <roseecm@gmail.com>
2025-12-13 09:55:45 -0500

    adding sample to blogimages

---
+++ client/src/data/blogImages.ts
@@ -0,0 +1,12 @@
+// client/src/data/blogImages.ts
+export const BLOG_DEFAULT_CARD = "/assets/img/blog/library/blog-list-hero.jpg";
+export const BLOG_DEFAULT_HERO = "/assets/img/blog/library/blog-list-hero.jpg";
+
+export const blogImages: Record<string, string> = {
+  // Example mappings (add more as you go)
+  "pro-tips-for-giving-tuesday": "/assets/img/blog/library/community-healing-diverse.png",
+
+  // Add your real slugs here:
+  // "alcohol-vs-weed-how-are-you-really-coping": "/assets/img/blog/library/blog-discussion.png",
+  // "healing-isnt-pretty-but-its-holy": "/assets/img/blog/library/miracle-moment.png",
+};
