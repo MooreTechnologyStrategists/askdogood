6178ab05d6f6ff5a5344533f0d4604ae0aa9d520
RoSeé Murphy <roseecm@gmail.com>
2025-12-13 07:24:14 -0500

    added image.ts

---
+++ client/src/lib/images.ts
@@ -0,0 +1,14 @@
+export const images = {
+  founder: {
+    home: "/assets/img/rosee-home.jpg",
+    about: "/assets/img/banners/about.jpg",
+    journey: "/assets/img/banners/journey.jpg",
+    contact: "/assets/img/banners/contact.jpg",
+    blog: "/assets/img/banners/blog.jpg",
+    login: "/assets/img/banners/login.jpg",
+  },
+
+  brand: {
+    placeholder: "/assets/img/banners/placeholder.jpg",
+  },
+};
